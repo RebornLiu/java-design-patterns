ce024c1305a106666452d82a73f22844478145f6
RebornLiu <1025474617@qq.com>
2018-01-24 23:56:06 +0800

    2018-01-24

---
+++ abstract-document/src/main/java/jdk6/Email.java
@@ -0,0 +1,13 @@
+package jdk6;
+
+/**
+ * @author Reborn
+ * @since 2018/1/24
+ */
+public interface Email {
+    String key = "key";
+
+    String getEmail();
+    void putEmail(String email);
+
+}
